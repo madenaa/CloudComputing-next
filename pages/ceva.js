4f96353b91feb78175df62edc507fd86c78d7902
madenaa <enachem21@gmail.com>
2023-05-14 01:59:33 +0300

    backend

---
+++ pages/ceva.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Ceva(props) {
+	return (
+		<div>CEVA</div>
+	);
+}
+
+export default Ceva;
---
+++ pages/nested/altceva.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ALTCEVA(props) {
+	return (
+		<div>ALTCEVA</div>
+	);
+}
+
+export default ALTCEVA;
