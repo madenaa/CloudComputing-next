fe2f89dfd0d60010cf130c7e31815195905f1d1e
madenaa <enachem21@gmail.com>
2023-05-15 22:56:55 +0300

    frontend init

--- js/components/MainPage.jsx
+++ js/components/MainPage.jsx
@@ -40,7 +40,7 @@ export default function MainPage() {
 		<section className={"bg-gradient-to-b from-blue-50 to-transparent dark:from-blue-900 w-full h-full absolute top-0 left-0 z-0"}>
             <div className={"container px-6 py-10 mx-auto"}>
                 <h1 className={"w-[500px] mx-auto text-center text-6xl font-bold text-sky-400 font-serif hover:font-sans w-[1000px] mx-auto text-center mt-9 "}>EventSnap</h1>
-                <p className={"w-[1000px] mx-auto text-center mt-4 text-3xl text-sky-200 font-ui-monospace"}> Never Miss a Moment, Embrace Every Event</p>
+                <p className={"w-[1000px] mx-auto text-center mt-4 text-3xl text-sky-200 font-serif hover:font-sans w-[1000px]"}> Never Miss a Moment, Embrace Every Event</p>
 
                 <div className={"grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3"}>
                     {
